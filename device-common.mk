2656b16447d023f28b80dac18bfc331d2e30882d
Flominator <github.flominatorgd@gmail.com>
2023-03-10 22:44:10 +0100

    exynos7870-common: kill oss bsp

--- device-common.mk
+++ device-common.mk
@@ -30,8 +30,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 -include $(LOCAL_PATH)/product/*.mk
 
 # Call Samsung LSI Board Support Package
-$(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
-$(call inherit-product, hardware/samsung_slsi-cm/exynos7870/exynos7870.mk)
+#$(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
+#$(call inherit-product, hardware/samsung_slsi-cm/exynos7870/exynos7870.mk)
 
 # call the common proprietary setup
 $(call inherit-product, vendor/samsung/exynos7870-common/exynos7870-common-vendor.mk)
