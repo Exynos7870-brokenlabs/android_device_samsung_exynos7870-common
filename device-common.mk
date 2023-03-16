1d6fdbd2d74aba531d3b59d08435068e48e4239b
Flominator <github.flominatorgd@gmail.com>
2023-03-16 20:58:41 +0100

    exynos7870-common: build android.hardware.sensors@1.0-service

--- device-common.mk
+++ device-common.mk
@@ -255,7 +255,11 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-impl
+
+# Vibrator
+PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service \
     android.hardware.vibrator@1.0-impl
 
